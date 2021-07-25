2316279adc13de99f6540ed7dd8c2778028d12c2
Benjamin L. Tischendorf <donkeybridge@jtown.eu>
2021-07-25 16:37:01 +0000

    bin/signals added :lookback parameter

--- bin/signals.rb
+++ bin/signals.rb
@@ -6,8 +6,9 @@ if ARGV[0].nil?
   raise ArgumentError, "Need symbol to continue"
 end
 symbol = ARGV[0].upcase
-report = ARGV[1].nil? ? :legacy : ARGV[1].downcase
-filter = ARGV[2].nil? ? nil : /_#{ARGV[2].downcase}_/
+lookback = ARGV[1].nil? ? 55 : ARGV[1].to_i
+report = ARGV[2].nil? ? :legacy : ARGV[2].downcase
+filter = ARGV[3].nil? ? nil : /_#{ARGV[3].downcase}_/
 
-s = Cotcube::CftcSource.signals symbol:  ARGV[0], report: report.to_sym, human: false, count: 800, quiet: true, filter: filter
+s = Cotcube::CftcSource.signals symbol: symbol, lookback: lookback, report: report.to_sym, human: false, count: 800, quiet: true, filter: filter
 s.each {|x| puts x} 
